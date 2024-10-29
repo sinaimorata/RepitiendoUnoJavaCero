b31e8bac5bcba6bf93e9d2b3ba3d40cb58a94f10
Montse Morata <sinaimh@gmail.com>
2024-10-29 12:50:48 +0100

    finalizado

--- src/uno/proyecto/javadesdecero/Test.java
+++ src/uno/proyecto/javadesdecero/Test.java
@@ -6,5 +6,12 @@ public class Test {
 		System.out.print("Hello Word");
 
 	}
-
+	public static void fuction() {
+		boolean a= true;
+		char character = 'a';
+		int i = 5;
+		i++; // i = i +1
+		i--; // i = i -1
+		
+	}
 }
